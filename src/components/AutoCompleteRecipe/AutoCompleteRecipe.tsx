59a19e1c866c9708b2dcdf88ebceb10c3025d42a
Mohit Kumar <agarwalmohit43@gmail.com>
2025-10-19 17:39:44 +0000

    AutoCompleteRecipe WIP

--- src/components/AutoCompleteRecipe/AutoCompleteRecipe.tsx
+++ src/components/AutoCompleteRecipe/AutoCompleteRecipe.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { RecipeInterface } from "../../typings/autoCompleteRecipe";
 import { RECIPE_SEARCH_URL } from "../../constants/autoCompleteRecipe";
 import { debounce } from "../../helpers";
@@ -7,12 +7,19 @@ import "./styles.css";
 const AutoCompleteRecipe = () => {
   const [recipeList, setRecipeList] = useState<RecipeInterface[]>([]);
   const [recipe, setRecipe] = useState<string>("");
+  const [showResults, setShowResults] = useState(false);
   const fetchRecipe = useCallback(async (recipe: string) => {
     const res = await fetch(RECIPE_SEARCH_URL(recipe)).then((res) =>
       res.json()
     );
 
-    setRecipeList(res);
+    if (res.recipes.length > 0) {
+      setRecipeList(res.recipes);
+      setShowResults(true);
+    } else {
+      setRecipeList([]);
+      setShowResults(false);
+    }
   }, []);
   const debouncedFetchRecipe = debounce(fetchRecipe, 1000);
   useEffect(() => {
@@ -26,6 +33,15 @@ const AutoCompleteRecipe = () => {
     setRecipe(value);
   };
 
+  const handleResultSelect = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLElement;
+    const spanTag = target.closest("span.search-item") as HTMLElement | null;
+    if (!spanTag) return;
+    const value = spanTag?.dataset?.value || "";
+    setRecipe(value);
+    setShowResults(false);
+  };
+
   return (
     <div>
       AutoCompleteRecipe
@@ -36,15 +52,23 @@ const AutoCompleteRecipe = () => {
           value={recipe}
           onChange={handleChangeRecipe}
         />
-        <div className="search-result">
-          {recipeList?.length > 0
-            ? recipeList.map((item: RecipeInterface) => {
-                console.log(item);
-
-                return <span key={item.id}>{item.name}</span>;
-              })
-            : null}
-        </div>
+        {showResults ? (
+          <div className="search-result" onClick={handleResultSelect}>
+            {recipeList?.length > 0
+              ? recipeList.map((item: RecipeInterface) => {
+                  return (
+                    <span
+                      key={item.id}
+                      className="search-item"
+                      data-value={item.name}
+                    >
+                      {item.name}
+                    </span>
+                  );
+                })
+              : null}
+          </div>
+        ) : null}
       </div>
     </div>
   );
